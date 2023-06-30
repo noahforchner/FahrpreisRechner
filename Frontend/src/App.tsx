ad1c15497f2bf60a05017fc36ea38681ebde2805
Noah Forchner <noah.forchner@hamburger-software.de>
2023-06-30 17:31:13 +0200

    Erste Funktionen für buttons

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import LoginMaske from './components/LoginMaske';
 import LandingPage from './components/LandingPage';
 import RegistrationMaske from './components/RegistrationMaske';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginMaske />} />
         <Route path='/registrierung' element={<RegistrationMaske />} />
+        <Route path="*" element={<ErrorPage/>}></Route>
       </Routes>
     </Router>
   );
---
+++ Frontend/src/components/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+import Background from '../assets/background2.0.png';
+
+function ErrorPage() {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Typography variant='h1'>ERROR! PAGE NOT FOUND</Typography>
+    </Box>
+  );
+}
+export default ErrorPage;
