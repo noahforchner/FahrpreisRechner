91f114b914ae1950c52c501f56d28f8c8536c523
j-j-martin <44782208+j-j-martin@users.noreply.github.com>
2023-07-07 21:42:44 +0200

    Map.tsx Code Refactor

--- Frontend/src/components/Map.tsx
+++ Frontend/src/components/Map.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import { LatLng } from 'use-places-autocomplete';
 import AddressInput from './AddressInput';
-import { useMemo } from 'react';
+
+const center = { lat: 53.52, lng: 10 };
+const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const parseLocation = (coordinates: LatLng | undefined) => {
+  if (typeof coordinates !== 'undefined') {
+    console.log(`${coordinates.lat},${coordinates.lng}`);
+    return `${coordinates.lat},${coordinates.lng}`;
+  }
+  return '';
+};
 
 const Map = () => {
-  const center = useMemo(() => ({ lat: 53.52, lng: 10 }), []);
   const [selectedStart, setSelectedStart] = useState<LatLng>();
   const [selectedDestination, setSelectedDestination] = useState<LatLng>();
   const [directionsResult, setDirectionsResult] = useState<google.maps.DirectionsResult>();
@@ -12,13 +21,17 @@ const Map = () => {
   const [routeDistance, setRouteDistance] = useState(0);
   const [routeDuration, setRouteDuration] = useState(0);
 
-  const parseLocation = (coordinates: LatLng | undefined) => {
-    if (typeof coordinates !== 'undefined') {
-      console.log(`${coordinates.lat},${coordinates.lng}`);
-      return `${coordinates.lat},${coordinates.lng}`;
-    }
-    return '';
-  };
+  const startLocation = parseLocation(selectedStart);
+  const destinationLocation = parseLocation(selectedDestination);
+
+  const embedUrl = `https://www.google.com/maps/embed/v1`;
+  const viewUrl = `${embedUrl}/view?key=${googleMapsApiKey}&center=${parseLocation(
+    center
+  )}&zoom=10`;
+  const placeUrl = `${embedUrl}/place?key=${googleMapsApiKey}&q=${
+    startLocation || destinationLocation
+  }`;
+  const directionsUrl = `${embedUrl}/directions?key=${googleMapsApiKey}&origin=${startLocation}&destination=${destinationLocation}&avoid=tolls|highways&mode=bicycling&units=metric`;
 
   return (
     <>
@@ -29,32 +42,12 @@ const Map = () => {
         width="600"
         height="450"
         loading="lazy"
-        //allowfullscreen
         src={
           selectedStart && selectedDestination
-            ? 'https://www.google.com/maps/embed/v1/directions' +
-              '?key=' +
-              import.meta.env.VITE_GOOGLE_MAPS_API_KEY +
-              '&origin=' +
-              parseLocation(selectedStart) +
-              '&destination=' +
-              parseLocation(selectedDestination) +
-              '&avoid=tolls|highways' +
-              '&mode=bicycling' +
-              '&units=metric'
-            : selectedStart || selectedDestination
-            ? 'https://www.google.com/maps/embed/v1/place' +
-              '?key=' +
-              import.meta.env.VITE_GOOGLE_MAPS_API_KEY +
-              '&q=' +
-              (selectedStart ? parseLocation(selectedStart) : parseLocation(selectedDestination)) +
-              '&q=Eiffel+Tower,Paris+France'
-            : 'https://www.google.com/maps/embed/v1/view' +
-              '?key=' +
-              import.meta.env.VITE_GOOGLE_MAPS_API_KEY +
-              '&center=' +
-              parseLocation(center) +
-              '&zoom=10'
+            ? directionsUrl
+            : startLocation || destinationLocation
+            ? placeUrl
+            : viewUrl
         }
       ></iframe>
     </>
